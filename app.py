242e295361139e9c91fcf2dd546c2b8aa48e3a3c
Patrick <gather4real@yahoo.com>
2020-12-29 16:12:03 +0000

    Add Login functionality

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def register():
 
         register = {
             "username": request.form.get("username").lower(),
-            "password": request.form.get("password")
+            "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
@@ -79,24 +79,24 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/profile/<username>", methods=["GET", "POST"])
-def profile(username):
-    # grab the session user's username from db
-    username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
+# @app.route("/profile/<username>", methods=["GET", "POST"])
+# def profile(username):
+#     # grab the session user's username from db
+#     username = mongo.db.users.find_one(
+#         {"username": session["user"]})["username"]
 
-    if session["user"]:
-        return render_template("profile.html", username=username)
+#     if session["user"]:
+#         return render_template("profile.html", username=username)
 
-    return redirect(url_for("login"))
+#     return redirect(url_for("login"))
     
 
-@app.route("/logout")
-def logout():
-    # remove user from session cookie
-    flash("You have been logged out")
-    session.pop("user")
-    return redirect(url_for("login"))
+# @app.route("/logout")
+# def logout():
+#     # remove user from session cookie
+#     flash("You have been logged out")
+#     session.pop("user")
+#     return redirect(url_for("login"))
 
 
 @app.route("/add_review", methods=["GET", "POST"])
